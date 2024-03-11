ea78d8955ea0d23f953abeac0c8ec86f5d89bddc
Konstantinos Pistolas <122966880+KPISTOLAS@users.noreply.github.com>
2024-03-11 18:11:58 +0200

    Add files via upload

---
+++ FileMaker/src/main/java/com/mycompany/filemaker/FileMaker.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.filemaker;
+
+/**
+ *
+ * @author kopisto
+ */
+public class FileMaker {
+
+    public static void main(String[] args) {
+        System.out.println("File Maker just started");
+        Frame1 Frame1 = new Frame1();
+        Frame1.setVisible(true);
+        
+    }
+
+    
+}
